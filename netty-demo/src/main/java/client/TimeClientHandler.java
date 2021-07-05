4555d421b8ed21706d7a1ebc0fa7dc551e108f01
rockit-ba <1121153008@qq.com>
2021-07-05 15:35:56 +0800

    'update'

--- netty-demo/src/main/java/client/TimeClientHandler.java
+++ netty-demo/src/main/java/client/TimeClientHandler.java
@@ -10,10 +10,14 @@ import java.util.Date;
  *  它与服务器端代码并没有真正的区别。
  *  那么ChannelHandler实现呢?它应该从服务器接收一个32位整数，
  *  将其转换为人类可读的格式，打印转换后的时间，并关闭连接
+ *
+ *  它看起来非常简单，与服务器端示例没有任何不同。
+ *  然而，这个处理程序有时会拒绝抛出IndexOutOfBoundsException。
 **/
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // 在TCP/IP协议中，Netty将从对等端发送的数据读入ByteBuf。
         ByteBuf m = (ByteBuf) msg;
         try {
             long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
